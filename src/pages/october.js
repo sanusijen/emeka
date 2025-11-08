f673b1da156e48f663872068ad3a4764444a97eb
sanusijen <89282336+sanusijen@users.noreply.github.com>
2025-11-08 15:59:37 +0100

    Update october.js

--- src/pages/october.js
+++ src/pages/october.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
 function October() {
   return (
     <section id="about" className="block about-block">
